44530b9f3cd1aea1708a2116c55a5ea0833d5bfe
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-01-28 22:30:18 +0100

    Import wielu klas naraz

--- oca/chapter1/defineclasses/MyClassWithImport.java
+++ oca/chapter1/defineclasses/MyClassWithImport.java
@@ -1,7 +1,8 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class MyClassWithImport {
 	public static void main(String[] args) {
 		ArrayList<String> a = new ArrayList<String>();
+		TreeSet<String> t = new TreeSet<String>();
 	}
 }
