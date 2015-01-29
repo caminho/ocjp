f00859df1c1ddbcf2e1a0c4dace1cafe2ccf073a
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-01-29 21:05:41 +0100

    Test dostępu domyślnego

---
+++ oca/chapter1/defineclasses/cert/Beverage.java
@@ -0,0 +1,7 @@
+package cert;
+
+class Beverage {
+	public static void main(String[] args) {
+		System.out.println("Jestem w klasie Beverage");
+	}
+}
---
+++ oca/chapter1/defineclasses/exam/stuff/Tea.java
@@ -0,0 +1,9 @@
+package exam.stuff;
+
+import cert.Beverage;
+
+class Tea extends Beverage {
+	public static void main(String[] args) {
+		System.out.println("Jestem w klasie Tea");
+	}
+}
