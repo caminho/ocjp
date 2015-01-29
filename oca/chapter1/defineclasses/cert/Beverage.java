562ffde1643a5577e2f162f03f2d20b1e5e61ad1
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-01-29 22:01:01 +0100

    Klasa finalna o dostępie publicznym

--- oca/chapter1/defineclasses/cert/Beverage.java
+++ oca/chapter1/defineclasses/cert/Beverage.java
@@ -1,6 +1,9 @@
 package cert;
 
-public class Beverage {
+public final class Beverage {
+	public void importantMethod() {
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Jestem w klasie Beverage");
 	}
