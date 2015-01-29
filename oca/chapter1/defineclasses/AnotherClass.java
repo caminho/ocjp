7d1883763f3b788a3e1172bd284de00d08774a85
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-01-29 22:23:03 +0100

    Klasa abstrakcyjna

---
+++ oca/chapter1/defineclasses/AnotherClass.java
@@ -0,0 +1,6 @@
+class AnotherClass {
+	public static void main(String[] args) {
+		// B³¹d - nie kompiluje siê
+		// Car car = new Car();  
+	}
+}
---
+++ oca/chapter1/defineclasses/Car.java
@@ -0,0 +1,8 @@
+abstract class Car {
+	private double price;
+	private String model;
+	private String year;
+	public abstract void goFast();
+	public abstract void goUpHill();
+	public abstract void impressneighbors();
+}
