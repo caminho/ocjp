dfd749808553e6c7fc3782324b6431fd3266f771
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-02-01 00:09:54 +0100

    Eksperymenty z final

---
+++ oca/chapter1/declareclassmembers/FinalMethods.java
@@ -0,0 +1,12 @@
+
+class SuperClass {
+	public final void showSample() {
+		System.out.println("One thing.");
+	}
+}
+
+class SubClass extends SuperClass {
+	public void showSample() {
+		System.out.println("Another thing.");
+	}
+}
