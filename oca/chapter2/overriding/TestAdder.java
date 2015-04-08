54d3280ca20ff749bade62a2fbe4375cc77b86a0
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-04-08 09:57:54 +0200

    Uporzadkowanie rozdzialu 2

---
+++ oca/chapter2/overriding/TestAdder.java
@@ -0,0 +1,19 @@
+class Adder {
+	public int addThem(int x, int y) {
+		return x + y;
+	}
+	
+	public double addThem(double x, double y) {
+		return x + y;
+	}
+}
+
+public class TestAdder {
+	public static void main(String[] args) {
+		Adder a = new Adder();
+		int b = 27;
+		int c = 3;
+		int result = a.addThem(b, c);
+		double doubleResult = a.addThem(22.5, 9.3);
+	}
+}
