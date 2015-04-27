a1797af3d2ef26d4eff23bb40a3828770ad13344
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-04-27 21:58:06 +0200

    Klasa StringBuilder

--- oca/chapter5/strings/StringBuilderTest.java
+++ oca/chapter5/strings/StringBuilderTest.java
@@ -1,5 +1,29 @@
 public class StringBuilderTest {
 	public static void main(String[] args) {
+	
+		StringBuilder sb = new StringBuilder("set ");
+		sb.append("point");
+		System.out.println(sb);
+		
+		sb = new StringBuilder("pi = ");
+		sb.append(3.14159265f);
+		System.out.println(sb);
+		
+		sb = new StringBuilder("0123456789");
+		sb.delete(4, 6);
+		System.out.println(sb);
+		
+		sb = new StringBuilder("01234567");
+		sb.insert(4, "---");
+		System.out.println(sb);
+		
+		sb = new StringBuilder("ABCDEF");
+		sb.reverse();
+		System.out.println(sb);
+		
+		sb = new StringBuilder("Test string");
+		System.out.println(sb.toString());
+		
 		
 	}
 }
