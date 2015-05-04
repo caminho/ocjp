f2dde2e7c61a26735928f6ca709660daf05be40d
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-05-04 22:07:32 +0200

    StringBuilder

--- oca/chapter5/strings/StringBuilderTest.java
+++ oca/chapter5/strings/StringBuilderTest.java
@@ -1,28 +1,27 @@
 public class StringBuilderTest {
 	public static void main(String[] args) {
 	
-		StringBuilder sb = new StringBuilder("set ");
-		sb.append("point");
-		System.out.println(sb);
+		StringBuilder sb = new StringBuilder();
+		sb.append("set ").append("point");
+		System.out.println("Result: [" + sb + "], capacity: " + sb.capacity());
 		
 		sb = new StringBuilder("pi = ");
 		sb.append(3.14159265f);
-		System.out.println(sb);
+
+		System.out.println("Result: [" + sb + "], capacity: " + sb.capacity());
 		
 		sb = new StringBuilder("0123456789");
 		sb.delete(4, 6);
-		System.out.println(sb);
+		System.out.println("Result: [" + sb + "], capacity: " + sb.capacity());
 		
 		sb = new StringBuilder("01234567");
 		sb.insert(4, "---");
-		System.out.println(sb);
+		System.out.println("Result: [" + sb + "], capacity: " + sb.capacity());
 		
-		sb = new StringBuilder("ABCDEF");
-		sb.reverse();
-		System.out.println(sb);
+		sb = new StringBuilder(0).append("ABCDEF");
+		System.out.println("Result: [" + sb.reverse() + "], capacity: " + sb.capacity());
 		
 		sb = new StringBuilder("Test string");
 		System.out.println(sb.toString());
-		
 	}
 }
