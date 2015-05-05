d5851af716dead87d6ac18475fe0c449d111c62a
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-05-05 23:50:13 +0200

    Tablice

---
+++ oca/chapter5/arrays/ArrayTest.java
@@ -0,0 +1,18 @@
+public class ArrayTest {
+	public static void main(String[] args) {
+		int[] key;
+		int[] tab1[], zmienna, tab3[][];
+		tab1 = new int[1][];
+		zmienna = new int[5];
+		tab3 = new int[2][][];
+		
+		float[][] f1, f2[], f3[][];
+		f1 = new float[1][];
+		f2 = new float[1][][];
+		f3 = new float[1][][][];
+		
+		Thread[] threads;
+		threads = new Thread[15];
+		Thread manyThreads[] = new Thread[16];
+	}
+}
