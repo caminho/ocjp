33bf28b3e9ba0295b44eecee25ea58b35b59d483
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-05-05 21:57:37 +0200

    StringBuilder c.d.

--- oca/chapter5/strings/StringBuilderTest.java
+++ oca/chapter5/strings/StringBuilderTest.java
@@ -23,5 +23,41 @@ public class StringBuilderTest {
 		
 		sb = new StringBuilder("Test string");
 		System.out.println(sb.toString());
+		
+		// trzy sposoby na utworzenie StringBuildera
+		sb = new StringBuilder(); // capacity = 16
+		System.out.println(sb.capacity());
+		
+		sb = new StringBuilder("abc"); // capacity = 16+3 = 19
+		System.out.println(sb.capacity());
+		
+		sb = new StringBuilder(5); // capacity = 5
+		System.out.println(sb.capacity());
+		
+		// trzy reguły dotyczące StringBuildera
+		System.out.println("1) append - zwieksza pojemnosc");
+		sb = new StringBuilder();
+		System.out.println(sb.capacity()); // 16
+		sb.append("0123456789");
+		System.out.println(sb.capacity()); // 16
+		sb.append("0123456");
+		System.out.println(sb.capacity()); // 34
+		System.out.println(sb);
+		
+		System.out.println("2) insert - zwieksza pojemnosc");
+		sb = new StringBuilder();
+		System.out.println(sb.capacity()); // 16
+		sb.insert(0, "0123456789");
+		System.out.println(sb.capacity()); // 16
+		sb.insert(8, "0123456789");
+		System.out.println(sb.capacity()); // 16
+		System.out.println(sb);
+		
+		System.out.println("3) insert - wyrzuca wyjatek, gdy wstawiamy poza napis");
+		sb = new StringBuilder();
+		System.out.println(sb.capacity()); // 16
+		sb.insert(0, "0123456789");
+		System.out.println(sb.capacity()); // 16
+		sb.insert(12, "01"); // StringIndexOutOfBoundsException
 	}
 }
