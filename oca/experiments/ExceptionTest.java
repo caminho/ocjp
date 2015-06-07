fe0352d25db4fd59c858959b61d443ad10a286d1
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2015-06-07 12:18:17 -0700

    Wyjątki - eksperymenty z kodem

--- oca/experiments/ExceptionTest.java
+++ oca/experiments/ExceptionTest.java
@@ -1,5 +1,15 @@
+import java.io.*;
+
 public class ExceptionTest {
 	public static void main(String[] args) {
+		
+		try {
+			throw new FileNotFoundException();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Koniec");
+		}		
 		
 	}
 }
